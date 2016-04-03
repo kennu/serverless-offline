e114677763aa62cecf9376164dd22a51414317db
Kenneth Falck <kennu@iki.fi>
2016-04-04 02:39:12 +0300

    Fix double-array issue with wrap:true

--- src/jsonPath.js
+++ src/jsonPath.js
@@ -4,10 +4,10 @@ const JSONPath = require('jsonpath-plus');
 const debugLog = require('./debugLog');
 
 module.exports = function jsonPath(json, path) {
-  
+
   debugLog('Calling JSONPath:', path);
-  const result = JSONPath({ json, path, wrap: false });
+  const result = JSONPath({ json, path, wrap: true })[0];
   debugLog('JSONPath resolved:', result);
-  
+
   return result;
 };
